ee00e5fccd8dff27d4e0beae907375df580e3110
Lexsonn <Daimen.n.estevez@outlook.com>
2018-10-02 22:36:27 -0400

    Setup for C++ patrolling

---
+++ TestingGrounds/Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
+	//Super::ExecuteTask(OwnerComp, NodeMemory);
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %s: %i"), *IndexKey.SelectedKeyName.ToString(), Index);
+	return EBTNodeResult::Succeeded;
+}
+
+
