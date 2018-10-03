cc34a45027685141c81e7b56a263b4ce8cec48aa
Lexsonn <Daimen.n.estevez@outlook.com>
2018-10-03 19:46:59 -0400

    Fully migrated Patrol to a Component in C++

--- TestingGrounds/Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ TestingGrounds/Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -1,13 +1,31 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
+#include "AIController.h"
+#include "AI/PatrolComponent.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
-	//Super::ExecuteTask(OwnerComp, NodeMemory);
+	// Get Patrol Points
+	auto AIPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolComponent = AIPawn->FindComponentByClass<UPatrolComponent>();
+	if (!PatrolComponent) return EBTNodeResult::Aborted;
+	TArray<AActor*> PatrolPoints = PatrolComponent->GetPatrolPoints();
+
+	if (PatrolPoints.Num() <= 0) {
+		UE_LOG(LogTemp, Warning, TEXT("No Patrol point set for %s"), *AIPawn->GetName());
+		return EBTNodeResult::Aborted;
+	}
+
+	// Set Next Waypoint
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
-	UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %s: %i"), *IndexKey.SelectedKeyName.ToString(), Index);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+	// UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %s: %i"), *IndexKey.SelectedKeyName.ToString(), Index);
+
+	// Cycle Index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
 	return EBTNodeResult::Succeeded;
 }
 
